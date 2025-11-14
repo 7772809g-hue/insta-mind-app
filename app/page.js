c9782f090e9d308ea62d5baf96dbdbc96c81ec13
7772809g-hue <7772809g@gmail.com>
2025-11-14 01:19:17 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontSize: "30px" }}>
+      <h1>Insta-Mind App</h1>
+      <p>Your AI Instagram assistant is working!</p>
+    </div>
+  );
+}
